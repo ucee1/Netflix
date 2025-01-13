ca9a47d5904c109547045ba6bdc1f9931208f0d7
uche <princeuzoma27@gmail.com>
2025-01-13 12:20:55 +0100

    initialcommit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// document.querySelectorAll('.hamburger').forEach(menu => {
+//     menu.addEventListener('click', handleClick);
+// });
+
+// function handleClick(event) {
+//     const targetId = event.target.getAttribute('jumbo-details');
+//     const jumboDetails = document.getElementById(targetId);
+
+//     console.log("Its clicking");
+
+//     if (jumboDetails.style.display === 'none' || jumboDetails.style.display === '') {
+//         jumboDetails.style.display = 'block';
+//         jumboDetails.style.transition = '.9s';
+//     } else {
+//         jumboDetails.style.display = 'none';
+//     }
+// }
